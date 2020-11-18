b816af68cb05ec8139902fe503871cac698ddd62
Nguyen Le <69196079+lecnguyenn@users.noreply.github.com>
2020-11-18 11:48:06 +0700

    Create HomeComponent.js

---
+++ HomeComponent.js
@@ -0,0 +1,18 @@
+import { FadeTransform } from 'react-animation-components';
+
+. . .
+
+            <FadeTransform
+                in
+                transformProps={{
+                    exitTransform: 'scale(0.5) translateY(-50%)'
+                }}>
+                <Card>
+                    <CardImg src={baseUrl + item.image} alt={item.name} />
+                    <CardBody>
+                    <CardTitle>{item.name}</CardTitle>
+                    {item.designation ? <CardSubtitle>{item.designation}</CardSubtitle> : null }
+                    <CardText>{item.description}</CardText>
+                    </CardBody>
+                </Card>
+            </FadeTransform>
